d2a845668bf7c702fd0ba4e3b8e23782155430c4
Christian MacMillan <christian.macmillan@b-reel.com>
2019-11-13 16:15:26 +0100

    work updated

--- components/sections/Work.vue
+++ components/sections/Work.vue
@@ -22,28 +22,25 @@
     const data = {
         title: "s-work:title",
         projects: [
-            { name: "s-work:project:evagher", url: "https://evagher.com" },
-            { name: "s-work:project:ourplanet", url: "https://www.ourplanet.com/en/explorable-globe/landing" },
-            { name: "s-work:project:xaviercusso", url: "https://xaviercusso.com" },
-            { name: "s-work:project:hublot", url: "https://www.b-reel.com/work/hublot-digital-boutique" },
-            { name: "s-work:project:loropiana", url: "https://uk.loropiana.com/en" },
-            { name: "s-work:project:gorillaz", url: "https://www.b-reel.com/work/client/gorillaz" },
-            { name: "s-work:project:chaos", url: "https://shop.chaos.club/" },
-            { name: "s-work:project:h2o", url: "http://www.h2o.es/#/en/" },
-            { name: "s-work:project:intorno", url: "http://intorno.xyz" },
-            { name: "s-work:separator:text", separator: true },
-            { name: "s-work:project:pei" },
-            { name: "s-work:project:maricastana" },
-            { name: "s-work:project:roma" }
+           { name: "s-work:project:roma", url: "https://romalevin.com/" },
+           { name: "s-work:project:pixel", url: "https://b-reel.com/work/google-pixel-wallpapers" },
+           { name: "s-work:project:evagher", url: "https://evagher.com/" },
+           { name: "s-work:project:ourplanet", url: "https://www.ourplanet.com/en/explorable-globe/landing/" },
+           { name: "s-work:project:xaviercusso", url: "https://xaviercusso.com" },
+           { name: "s-work:project:loropiana", url: "https://uk.loropiana.com/en" },
+           { name: "s-work:project:hublot", url: "https://www.b-reel.com/work/hublot-digital-boutique" },
+           { name: "s-work:project:gorillaz", url: "https://www.b-reel.com/work/client/gorillaz" },
+           { name: "s-work:project:chaos", url: "https://shop.chaos.club/" },
+           { name: "s-work:project:h2o", url: "http://www.h2o.es/#/en/" },
         ],
-        clientTitle: "s-work:clients:title",
-        clients: [
-            { name: "WWF", url: "https://www.worldwildlife.org/" },
-            { name: "Google", url: "https://google.com" },
-            { name: "Netflix", url: "https://netflix.com" },
-            { name: "Hublot", url: "https://hublot.com" },
-            { name: "Mango", url: "https://mango.com" },
-            { name: "Audi", url: "https://www.audi.com/en.html" }
+        agencyTitle: "s-work:client:title",
+        agencies: [
+            { name: "s-work:client:google", url: "#" },
+            { name: "s-work:client:nike", url: "#" },
+            { name: "s-work:client:netflix", url: "#" },
+            { name: "s-work:client:wwf", url: "#" },
+            { name: "s-work:client:hublot", url: "#" },
+            { name: "s-work:client:gorillaz", url: "#" },
         ]
     }
 
